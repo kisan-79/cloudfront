bd2f63a6e8193f364b213eead94421f6691df20d
kisan-79 <kanabarkarkisan56@gmail.com>
2023-10-20 12:59:17 +0530

    cdn signed url generator

---
+++ src/main/java/com/runner/CloudFront.java
@@ -0,0 +1,30 @@
+package com.runner;
+
+import com.amazonaws.services.cloudfront.CloudFrontUrlSigner;
+import com.amazonaws.services.cloudfront.util.SignerUtils.Protocol;
+import java.io.File;
+import java.io.IOException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Date;
+
+public class CloudFront {
+
+    public static void main(String[] args) throws IOException, InvalidKeySpecException {
+        Protocol protocol = Protocol.https;
+        String distributionDomain = "d1ymlw8kq11tcy.cloudfront.net";
+        String s3ObjectKey = "sample.png";
+        //all 3 creates cloudfront distribution private url i.e. https://d1ymlw8kq11tcy.cloudfront.net/sample.png
+
+        File privateKeyFile = new File("/home/kisan/intelj-workspace/cloudfront/cloudfront.pem"); // private key corresponding to public key attached to distribution
+        String keyPairId = "KW1PC63HP23SN"; // id of public key attached to distribution
+
+
+        long fiveMinutesInMillis = 5 * 60 * 1000; // 5 minutes in milliseconds
+        Date expirationTime = new Date(System.currentTimeMillis() + fiveMinutesInMillis);
+
+
+        String url1 = CloudFrontUrlSigner.getSignedURLWithCannedPolicy(protocol, distributionDomain, privateKeyFile, s3ObjectKey, keyPairId, expirationTime);
+
+        System.out.println(url1);
+    }
+}
